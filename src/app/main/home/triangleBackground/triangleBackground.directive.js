c8699b4b45f157caae34a75b9e2ff5dea4d7856f
pbellon <bellon.pierre@gmail.com>
2016-07-20 17:06:05 +0200

    Move areaChart & triangle directives to main/home
    Those directive are only used by home template so it makes sense to put
    them here.

--- src/app/main/home/triangleBackground/triangleBackground.directive.js
+++ src/app/main/home/triangleBackground/triangleBackground.directive.js
@@ -26,7 +26,7 @@ class TriangleBackgroundController {
         this.vertices = [];
         let $el = d3.select(this.$element[0]);
         this.width =  this.$element.width();
-        this.height = this.$element.height() + 110;
+        this.height = this.$element.height() + 75;
 
         if(this.$svg){
             this.$svg.select('g').remove();
