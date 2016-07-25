ff4c7358f290f0a6c7aab89bfe04e322cf60958d
pbellon <bellon.pierre@gmail.com>
2016-07-25 18:05:52 +0200

    Improve agency page
    Now this page has a new way of presenting people (more nicely &
    originaly). It also now includes the triangleChart used to display our
    working process.

--- src/app/main/agency/agency.constant.js
+++ src/app/main/agency/agency.constant.js
@@ -1,27 +1,64 @@
 export var MEMBERS = [
     {
+        linkedin: 'https://fr.linkedin.com/in/lucaspiessat',
+        color: 'bh-yellow',
         name: 'Lucas Piessat',
-        title: 'Datajournaliste / CEO',
+        title: 'Datajournaliste',
         photo: '../assets/images/photos/business.jpeg',
-        description: `Ex- Sciences Po Grenoble et Grenoble EM.
+        function: 'L\'information, son architecture et sa mise en récit',
+        description: `Ex-Sciences Po Grenoble et Grenoble EM.
         J’ai été datajournaliste chez WeDoData et stagiaire
         au Monde Diplomatique. Je cherche à “rendre intéressant
         ce qui est important”, non l’inverse.`
     },
     {
+        linkedin: 'https://fr.linkedin.com/in/pierre-bellon-64b2ba3a',
+        color: 'bh-blue',
         name: 'Pierre Bellon',
-        title: 'Développeur / CTO',
+        title: 'Développeur/Designer',
         photo: '../assets/images/photos/business2.jpeg',
+        function: 'La conception et la réalisation d\'interface de données',
         description: `Ex-développeur chez J++ (agence de journalisme de données),
         ex-consultant au HCR. Je conçois et développe des outils
         et services web à forte valeur démocratique ajoutée.`
     },
     {
+        linkedin: 'https://fr.linkedin.com/in/gauthier-bravais-16815055',
+        color: 'bh-orange',
         name: 'Gauthier Bravais',
-        title: 'Chef de projet / COO',
+        title: 'Chef de projet',
         photo: '../assets/images/photos/business3.jpeg',
+        function: 'La communication, la stratégie et la gestion de projet',
         description: `Ex-Sciences Po Grenoble, j’ai trois ans d’expérience en
         agence de communication et relations publiques, en charge d’un
         portefeuille de 5 clients dans le numérique et l’innovation.`
+    }
+];
+
+export var PREVIOUS_WORK = [
+    {
+        title: 'Jeu d\'Influences',
+        subtitle: 'Serious Game développé au sein de J++',
+        picture: '../assets/images/previous-work/jeu-d-influences.png'
+    },
+    // {
+    //     title: 'Municipaly',
+    //     subtitle: 'Serious Game éditorialisé au sein de WeDoData',
+    //     picture: '../assets/images/previous-work/jeu-d-influences.png'
+    // },
+    // {
+    //     title: 'The Global Carbon Atlas',
+    //     subtitle: 'Visualisation de données interactive éditée au sein de WeDoData',
+    //     picture: '../assets/images/previous-work/jeu-d-influences.png'
+    // },
+    {
+        title: 'World of Work',
+        subtitle: 'Étude ludifiée développé au sein de J++',
+        picture: '../assets/images/previous-work/world-of-work.png'
+    },
+    {
+        title: 'Detective.io',
+        subtitle: 'Outil d\'investigation développé au sein de J++',
+        picture: '../assets/images/previous-work/detective-io.png'
     }
 ];
--- src/app/main/agency/agency.controller.js
+++ src/app/main/agency/agency.controller.js
@@ -1,7 +1,52 @@
 export class AgencyController {
-    constructor (MEMBERS) {
+    constructor (MEMBERS, PREVIOUS_WORK) {
         'ngInject';
+        this.chartData = [
+            {
+                name: 'Appropriation',
+                value: 19,
+                description: `Nous venons à votre rencontre et nous impliquons
+                fortement pour apprivoiser au mieux votre objet de recherche,
+                vos méthodes de travail et vos enjeux.`
+            },{
+                name: 'Clarification',
+                value: 17,
+                description: `À partir des travaux de recherche, nous élaborons
+                un discours accessible à tous. Sans le déformer, nous mettons en
+                récit le savoir pour le rendre universel.`
+            },{
+                name: 'Co-design',
+                value: 15,
+                description: `En associant notre savoir-faire et votre expertise,
+                nous travaillons ensemble à la conception et au design d’une interface
+                de restitution sur-mesure.`
+            },{
+                name: 'Développement',
+                value: 25,
+                description: `Nous mettons en oeuvre notre expertise technique
+                pour développer et maintenir des interfaces et des outils fiables
+                et performants.`
+            },{
+                name: 'Diffusion',
+                value: 20,
+                description: `Nous mettons en oeuvre des stratégies de
+                communication (réseaux sociaux, relations publiques et presse)
+                pour optimiser la diffusion de nos réalisations.`
+            }
+        ];
         this.members = MEMBERS;
+        this.previousWork = PREVIOUS_WORK;
     }
     listMembers(){ return this.members; }
+    listPreviousWork(){ return this.previousWork; }
+
+    getMember(name){
+        return this.members.find(function(m){
+            return m.name.toLowerCase().indexOf(name) !== -1
+        });
+    }
+
+    chartData(){
+        return
+    }
 }
