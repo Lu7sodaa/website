a42a0f038761b15ce25f38a9fa322b6569948cb7
pbellon <bellon.pierre@gmail.com>
2016-08-05 09:59:40 +0200

    Add reveal behavior on navbar (for home only)
    Set the same background color as for home header too

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -4,6 +4,9 @@ export function NavbarDirective() {
   let directive = {
     restrict: 'E',
     replace: true,
+    scope: {
+        'hide': '@'
+    },
     templateUrl: 'app/components/navbar/navbar.html',
     controller: NavbarController,
     controllerAs: 'nav',
@@ -14,13 +17,28 @@ export function NavbarDirective() {
 }
 
 class NavbarController {
-    constructor ($log, $rootScope, EVENTS) {
+    constructor ($log, $rootScope,$element,$document,$window, EVENTS) {
         'ngInject';
         this.$log = $log;
         this.sidebar_opened = false;
         this.EVENTS = EVENTS;
         this.$rootScope = $rootScope;
         this.$state = $rootScope.$state;
+        this.$nav = angular.element($element[0]);
+        this.revealed_class = 'revealed';
+        let w = angular.element($window);
+        if(this.hide){
+            let scroll_top_trigger = w.height() - 60;
+            // console.log('scroll_top_trigger', scroll_top_trigger);
+            this.$scrollContainer = angular.element('body');
+            $document.bind('scroll', ()=>{
+                let scroll_top = $document.scrollTop();
+                let trigger = scroll_top >= scroll_top_trigger - 60;
+                this.$nav.toggleClass(this.revealed_class, trigger);
+            });
+        } else {
+            this.$nav.addClass(this.revealed_class);
+        }
 
         this.handler = this.$rootScope.$watch('sidebar_opened', (newVal)=>{
             this.sidebar_opened = newVal;
