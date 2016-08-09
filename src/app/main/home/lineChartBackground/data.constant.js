d0963b03559e6822467016fe733ea5e90d7a9f82
pbellon <bellon.pierre@gmail.com>
2016-08-09 14:29:13 +0200

    Moved lineChartBackground data to new constant file

---
+++ src/app/main/home/lineChartBackground/data.constant.js
@@ -0,0 +1,33 @@
+export var LINE_CHART_DATA = [
+    {
+        animated: false,
+        values: [ 12, 10, 5, 8, 11, 13, 15, 16 ]
+    },{
+        animated: false,
+        values: [ 13, 14, 16, 16, 17, 18, 20, 21 ]
+    },{
+        animated: false,
+        values: [ 20, 19, 19, 21, 19, 17, 15, 14 ]
+    },{
+        animated: false,
+        values: [ 15, 12, 17, 13, 12, 14, 12, 11 ]
+    },{
+        animated: false,
+        values: [ 11, 12, 9, 7, 8, 10, 10, 11 ]
+    },{
+        animated: true,
+        animation_delay: 1500,
+        color_class: 'interfaces-color',
+        values: [ 8, 12, 10.5, 16, 21, 18, 19, 18 ]
+    },{
+        animated: true,
+        animation_delay: 1000,
+        color_class: 'design-color',
+        values: [ 13, 18, 16.5, 13, 14, 11, 15, 17 ]
+    },{
+        animated: true,
+        animation_delay: 700,
+        color_class: 'data-color',
+        values: [ 10, 9, 8, 10, 15, 17, 17, 18 ]
+    }
+];
--- src/app/main/home/lineChartBackground/lineChartBackground.directive.js
+++ src/app/main/home/lineChartBackground/lineChartBackground.directive.js
@@ -1,42 +1,3 @@
-var DATA = [
-    {
-        animated: false,
-        color_class: 'light-grey',
-        values: [ 12, 10, 5, 8, 11, 13, 15, 16 ]
-    },{
-        animated: false,
-        color_class: 'light-grey',
-        values: [ 13, 14, 16, 16, 17, 18, 20, 21 ]
-    },{
-        animated: false,
-        color_class: 'light-grey',
-        values: [ 20, 19, 19, 21, 19, 17, 15, 14 ]
-    },{
-        animated: false,
-        color_class: 'light-grey',
-        values: [ 15, 12, 17, 13, 12, 14, 12, 11 ]
-    },{
-        animated: false,
-        color_class: 'light-grey',
-        values: [ 11, 12, 9, 7, 8, 10, 10, 11 ]
-    },{
-        animated: true,
-        animation_delay: 1500,
-        color_class: 'interfaces-color',
-        values: [ 8, 12, 10.5, 16, 21, 18, 19, 18 ]
-    },{
-        animated: true,
-        animation_delay: 1000,
-        color_class: 'design-color',
-        values: [ 13, 18, 16.5, 13, 14, 11, 15, 17 ]
-    },{
-        animated: true,
-        animation_delay: 700,
-        color_class: 'data-color',
-        values: [ 10, 9, 8, 10, 15, 17, 17, 18 ]
-    }
-];
-
 export function LineChartBackgroundDirective(){
     let directive = {
         restrict: 'C',
@@ -48,10 +9,10 @@ export function LineChartBackgroundDirective(){
 
 
 class LineChartBackgroundController {
-    constructor($window, $element, $scope, $timeout, $log){
+    constructor($window, $element, $scope, $timeout, $log, LINE_CHART_DATA){
         'ngInject';
         this.$log = $log;
-        this.data = DATA;
+        this.data = LINE_CHART_DATA;
         this.$scope = $scope;
         this.$window = angular.element($window);
         this.$origin_el = angular.element($element[0]);
