833f0d43c50ab1830a508e1e745cfc752feda575
pbellon <bellon.pierre@gmail.com>
2016-08-10 18:39:21 +0200

    Remove commented code in index.run

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -2,21 +2,6 @@
 
 export function runBlock ($log, $rootScope, $state, $stateParams) {
     'ngInject';
-    //
-    // function onStateChangeSucces(e,toState, fromState, fromParams, toParams, opts){
-    //     $log.log(opts, toParams);
-    //     if(!toParams || !toParams.scroll_top){ return; }
-    //     let target = angular.element('nav');
-    //     if(toState.name === 'offer'){
-    //         target = angular.element('.main__header + section');
-    //     }
-    //     let scroll_top = target.offset().top;
-    //     $log.log('target', target, 'scroll_top', scroll_top);
-    //     angular.element('body,html').scrollTop(scroll_top);
-    //
-    // }
-    //
-    // var _handler = $rootScope.$on('$stateChangeSuccess', onStateChangeSucces);
 
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
