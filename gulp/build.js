01c89c152651db28d1340a248d8ebc4f4757315b
pbellon <bellon.pierre@gmail.com>
2017-01-16 18:51:28 +0100

    Rename siteWebV0 into skoli

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', ['markups'], function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'siteWebV0',
+      module: 'skoli',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
 
     ngHtml2JsPreprocessor: {
       stripPrefix: '(' + conf.paths.src + '/|' + conf.paths.tmp + '/serve/)',
-      moduleName: 'siteWebV0'
+      moduleName: 'skoli'
     },
 
     logLevel: 'WARN',
