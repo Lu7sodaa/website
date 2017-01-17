8908e455a0db426cf5127f35c184887591e47183
pbellon <bellon.pierre@gmail.com>
2017-01-17 18:58:21 +0100

    Add stupid directive generator in manage.sh

--- manage.sh
+++ manage.sh
@@ -4,6 +4,20 @@ run(){
     gulp serve
 }
 
+newDirective(){
+    #  args
+    # 1 - name
+    basePath=src/app/
+    name=${1:-newDirective}
+    dirPath=${2:-"$basePath/components"}
+    dirPath=$dirPath/$name
+    mkdir $dirPath
+    touch $dirPath/$name.html
+    touch $dirPath/$name.directive.js
+    touch $dirPath/$name.less
+}
+
+
 deploy(){
     gulp deploy
 }
