05f275bdad702438e8c4246a371c1120729a196e
pbellon <bellon.pierre@gmail.com>
2017-01-25 10:56:10 +0100

    Commented medium article (not published yet)

--- src/app/components/constants/news.js
+++ src/app/components/constants/news.js
@@ -5,11 +5,11 @@ export const NEWS_TYPES = {
 };
 // global news object
 export var NEWS = [
-    {
-        date: '2017-01-20',
-        type: NEWS_TYPES.article,
-        title: 'How to detect doublon in a large corpus?',
-        source: 'Medium',
-        url: 'http://medium.com/article'
-    }
+    // {
+    //     date: '2017-01-20',
+    //     type: NEWS_TYPES.article,
+    //     title: 'How to detect doublon in a large corpus?',
+    //     source: 'Medium',
+    //     url: 'http://medium.com/article'
+    // }
 ];
