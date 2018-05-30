b27a81b72cf128fa9dc603d214c9bf0c9affa895
Lu7sodaa <l.piessat@gmail.com>
2018-05-30 10:24:35 +0200

    updating news

--- src/app/components/constants/news.js
+++ src/app/components/constants/news.js
@@ -11,5 +11,26 @@ export var NEWS = [
         title: 'Construire, valoriser et entremettre les savoirs',
         source: 'Medium',
         url: 'https://medium.com/@AgenceSkoli/construire-valoriser-et-entremettre-les-savoirs-f0971224c76f#.smpwnl9tr'
+    },
+    {
+        date: '2018-04-30',
+        type: NEWS_TYPES.article,
+        title: 'Déplier les données — De l’usage des données dans le champ journalistique',
+        source: 'Medium',
+        url: 'https://medium.com/@AgenceSkoli/d%C3%A9plier-les-donn%C3%A9es-28fb2f62fa88'
+    },
+    {
+        date: '2018-04-28',
+        type: NEWS_TYPES.event,
+        title: 'Intervention de Skoli dans le cadre du WebComeLyon, le OFF de The WebConf2018',
+        source: 'The WebConf2018',
+        url: 'https://webcomelyon.fr/'
+    },
+    {
+        date: '2018-06-21',
+        type: NEWS_TYPES.event,
+        title: 'Humanitarian Encyclopedia – Practitioners workshop',
+        source: 'Humanitarian Encyclopedia',
+        url: 'https://humanitarianencyclopedia.org/2018/05/09/humanitarian-encyclopedia-practitioners-workshop/'
     }
 ];
